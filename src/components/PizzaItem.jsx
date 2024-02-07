7960a494d1433eebbb6c7686956365302ab620f6
Jhoana Espana <www.jhoanaespaa.com>
2024-02-07 16:21:30 -0500

    feature(PizzaMenu): add styles conditionals

--- src/components/PizzaItem.jsx
+++ src/components/PizzaItem.jsx
@@ -1,15 +1,15 @@
 import styles from './pizzaItem.module.css'
 
-const PizzaItem = ({ ingredients, name, photo, price }) => {
+const PizzaItem = ({ ingredients, name, photo, price, soldOut }) => {
   return (
-    <div className={styles.item_container}>
+    <div className={soldOut ? styles.sold_out : styles.item_container}>
       <div className={styles.item_img}>
         <img src={photo} alt='spinaci' />
       </div>
       <div className={styles.item_content}>
         <div className={styles.item_content_title}>
           <h2>{name}</h2>
-          <h3>${price}</h3>
+          <h3>{soldOut ? 'Sold out' : price}</h3>
         </div>
         <div className={styles.item_content_text}>
           <p>{ingredients}</p>
--- src/components/PizzaMenu.jsx
+++ src/components/PizzaMenu.jsx
@@ -11,15 +11,20 @@ const PizzaMenu = () => {
         <h3>Our menu</h3>
       </div>
       <div className={styles.items_container}>
-        {data.map((pizza) => (
-          <PizzaItem
-            key={pizza.id}
-            ingredients={pizza.ingredients}
-            name={pizza.name}
-            photo={pizza.photoName}
-            price={pizza.price}
-          />
-        ))}
+        {data.length > 0 ? (
+          data.map((pizza) => (
+            <PizzaItem
+              key={pizza.id}
+              ingredients={pizza.ingredients}
+              name={pizza.name}
+              photo={pizza.photoName}
+              price={pizza.price}
+              soldOut={pizza.soldOut}
+            />
+          ))
+        ) : (
+          <p>We're still working on our menu, Please come back later :)</p>
+        )}
       </div>
     </>
   )
